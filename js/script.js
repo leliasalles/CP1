77780463e4db3667fc152d0e63567984ee6a9812
Lélia Salles <89154346+leliasalles@users.noreply.github.com>
2021-11-11 23:21:35 -0300

    upload js

---
+++ js/script.js
@@ -0,0 +1,60 @@
+let sol = document.querySelector('sol');
+let fa = document.querySelector('fa');
+let doh = document.querySelector('doh');
+
+let descricao = document.getElementById('descricao');
+
+
+
+
+function userInput(){
+    document.getElementById('userTxt').outerHTML
+}
+
+
+
+function enviar(){
+    if(document.sol.checked == true && userInput == true){
+
+
+
+        
+    }else if(document.getElementById('fa').checked == true && userInput == true){
+
+
+
+    }else if(document.getElementById('do').checked == true && userInput == true){
+
+
+
+    }else{
+
+    }
+   
+}
+
+   
+
+
+function resetar(){
+
+    document.getElementById('resetar').innerHTML = "";
+
+}
+
+
+
+/*function enviar((document.getElementById('sol').checked == false && document.getElementById('fa').checked == false && document.getElementById('do').checked == false)){
+
+    if(document.getElementById('sol').checked == false && document.getElementById('fa').checked == false && document.getElementById('do').checked == false){
+
+        alert("Escolha uma opção")
+
+        
+
+    }else{
+
+     
+    }
+
+ */  
